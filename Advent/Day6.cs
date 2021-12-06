fd1cc2e2ef0e206806422a49fe12607697965b3c
EttienneS <ettiennes@live.co.za>
2021-12-06 19:36:01 +0200

    final cleanup day 6

--- Advent/Day6.cs
+++ Advent/Day6.cs
@@ -1,70 +1,74 @@
-﻿using Advent;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 
-internal class Day6 : ISolution
+namespace Advent
 {
     /// <summary>
     /// https://adventofcode.com/2021/day/6
     /// </summary>
-    public void Part1()
+    internal class Day6 : ISolution
     {
-        // naive approach:
-        var input = File.ReadAllText("./inputs/day6.txt");
-        var values = input.Split(',').Select(int.Parse).ToList();
+        private readonly Stopwatch _sw;
+        private readonly List<int> _values;
 
-        CalculateSpawn(values, 80);
-    }
-
-
-    public void Part2()
-    {
-        var input = File.ReadAllText("./inputs/day6.txt");
-        var values = input.Split(',').Select(int.Parse).ToList();
-
-        CalculateSpawn(values, 256);
-    }
-
-    private static void CalculateSpawn(List<int> values, int days)
-    {
-        var sw = new Stopwatch();
-        sw.Start();
-        var fish = new Dictionary<int, long>();
-        var lifespan = 9;
-        foreach (var value in values)
+        public Day6()
         {
-            IncrementDictionary(fish, value, 1);
+            _values = File.ReadAllText("./inputs/day6.txt").Split(',').Select(int.Parse).ToList();
+            _sw = new Stopwatch();
         }
 
-        for (int i = 0; i < days; i++)
+        public void Part1()
         {
-            var newFish = new Dictionary<int, long>();
-            for (int j = 0; j < lifespan; j++)
+            CalculateSpawn(_values, 80);
+        }
+
+        public void Part2()
+        {
+            CalculateSpawn(_values, 256);
+        }
+
+        private static void IncrementDictionary(Dictionary<int, long> dict, int index, long amount)
+        {
+            if (!dict.ContainsKey(index))
             {
-                if (!fish.ContainsKey(j)) continue;
-                var current = fish[j];
-                if (j == 0)
-                {
-                    IncrementDictionary(newFish, 6, current);
-                    IncrementDictionary(newFish, 8, current);
-                }
-                else
-                {
-                    IncrementDictionary(newFish, j - 1, current);
-                }
+                dict.Add(index, 0);
+            }
+            dict[index] += amount;
+        }
+
+        private void CalculateSpawn(List<int> values, int days)
+        {
+            _sw.Restart();
+
+            var fish = new Dictionary<int, long>();
+            foreach (var value in values)
+            {
+                IncrementDictionary(fish, value, 1);
             }
 
-            fish = newFish;
-        }
+            for (int i = 0; i < days; i++)
+            {
+                var newFish = new Dictionary<int, long>();
+                const int lifespan = 9;
+                for (int j = 0; j < lifespan; j++)
+                {
+                    if (!fish.ContainsKey(j)) continue;
+                    var current = fish[j];
+                    if (j == 0)
+                    {
+                        IncrementDictionary(newFish, 6, current);
+                        IncrementDictionary(newFish, 8, current);
+                    }
+                    else
+                    {
+                        IncrementDictionary(newFish, j - 1, current);
+                    }
+                }
 
-        Console.WriteLine($"{days} days calculated in {sw.ElapsedMilliseconds}ms\nTotal: {fish.Select(f => f.Value).Sum()}");
-    }
+                fish = newFish;
+            }
 
-    public static void IncrementDictionary(Dictionary<int, long> dict, int index, long amount)
-    {
-        if (!dict.ContainsKey(index))
-        {
-            dict.Add(index, 0);
+            Console.WriteLine($"{days} days calculated in {_sw.ElapsedMilliseconds}ms");
+            Console.WriteLine($"Total: {fish.Select(f => f.Value).Sum()}");
         }
-        dict[index] += amount;
     }
 }
